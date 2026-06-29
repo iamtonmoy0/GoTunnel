bc56d67de3c3a7461719d180959ef2b951ec4eb0
Tonmoy <tonmoy4671@gmail.com>
2026-06-29 21:15:57 +0600

    server initialized

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 func main() {
+	listener, err := net.Listen(
+		"tcp", ":9000")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("GoTunnel server running on port :9000")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		fmt.Println("New tunnel connected")
+		go handle(conn)
+	}
+
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			return
+		}
+		fmt.Println(string(buffer[:n]))
+	}
 
 }
