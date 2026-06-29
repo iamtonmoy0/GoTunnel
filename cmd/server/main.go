551dc87aa4c42dc1db0235335c2295b7e295d678
Tonmoy <tonmoy4671@gmail.com>
2026-06-29 22:25:16 +0600

    ref:server modified

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 	"github.com/iamtonmoy0/gotunnel/internal/tunnel"
 	"net"
+	"sync"
+)
+
+var (
+	clients   []net.Conn
+	clientsMu sync.Mutex
 )
 
 func main() {
@@ -12,22 +18,23 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer listener.Close()
 	fmt.Println("GoTunnel server running on port :9000")
-
+	fmt.Println("waiting for connections...")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			continue
 		}
-		fmt.Println("New tunnel connected")
-		go handle(conn)
+		fmt.Println("new connection from:", conn.RemoteAddr())
+		go handleConnection(conn)
 	}
 
 }
 
-func handle(user net.Conn) {
+func handleConnection(user net.Conn) {
 	defer user.Close()
-
+	//
 	client, err := net.Dial("tcp", "127.0.0.1:9000")
 	if err != nil {
 		fmt.Println(err)
